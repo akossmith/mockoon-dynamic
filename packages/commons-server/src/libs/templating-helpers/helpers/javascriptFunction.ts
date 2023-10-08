35394399332baed789d7762b9ad2c5472b75de0d
Akos Kovacs <akossmith@users.noreply.github.com>
2023-10-09 01:14:22 +0200

    add live-plugin-manager

--- packages/commons-server/src/libs/templating-helpers/helpers/javascriptFunction.ts
+++ packages/commons-server/src/libs/templating-helpers/helpers/javascriptFunction.ts
@@ -1,3 +1,7 @@
+import { PluginManager } from 'live-plugin-manager';
+
+const manager = new PluginManager();
+
 const javascriptFunction = function (fnString, ...args: any[]) {
     // eslint-disable-next-line no-eval
     return eval('(' + fnString + ')')(...args);
